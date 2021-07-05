974f320b09b61e1325a696f8ef5b1b84e7fe387f
monibBormon <monibbormon14@gmail.com>
2021-07-05 10:02:22 +0600

    advanced css3

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+(function ($){
+"use strict";
+
+
+
+
+ /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#intro').parallax("100%", 0.1);
+    $('#overview').parallax("100%", 0.3);
+    $('#detail').parallax("100%", 0.2);
+    $('#video').parallax("100%", 0.3);
+    $('#speakers').parallax("100%", 0.1);
+    $('#program').parallax("100%", 0.2);
+    $('#register').parallax("100%", 0.1);
+    $('#faq').parallax("100%", 0.3);
+    $('#venue').parallax("100%", 0.1);
+    $('#sponsors').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.2);
+
+  }
+  initParallax();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+})(jQuery);
